e4286eced7259004de5c7abb6a18c13be016e658
Ahmed Md Farhad <af31k@outlook.com>
2022-01-18 23:01:13 -0800

    Update sql_queries.py

--- Cloud_Data_Warehouse/Project_Cloud_Data_Warehouse/sql_queries.py
+++ Cloud_Data_Warehouse/Project_Cloud_Data_Warehouse/sql_queries.py
@@ -17,7 +17,7 @@ artist_table_drop = "DROP TABLE IF EXISTS artists;"
 time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
-#============================================================================
+#=================================================================================
 # CREATE TABLES
 # stagin_events is the log dataset
 staging_events_table_create= ("""
@@ -47,6 +47,7 @@ staging_events_table_create= ("""
         userId         INTEGER
     )
 """)
+
 # staging_songs are song dataset
 staging_songs_table_create = ("""
     CREATE TABLE IF NOT EXISTS staging_songs (
@@ -64,8 +65,10 @@ staging_songs_table_create = ("""
         year             INTEGER
     )
 """)
+
 #=============================================================
 # songplay table ( FACT TABLE )
+# songplay schema design
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays (
         songplay_id INT  IDENTITY(0,1) SORTKEY,
@@ -82,7 +85,8 @@ songplay_table_create = ("""
     );
 """)
 
-# [ DIMENSION TABLES ]-----------------
+#------------[ DIMENSION TABLES ]----------------------
+# users table schema
 user_table_create = ("""
     CREATE TABLE IF NOT EXISTS users (
         user_id       INTEGER      SORTKEY,
@@ -94,6 +98,7 @@ user_table_create = ("""
 """)
 
 # song table creation ( songs in music database )
+# song table schema
 song_table_create = ("""
     CREATE TABLE IF NOT EXISTS songs (
         song_id     VARCHAR(50)   SORTKEY,
@@ -105,6 +110,7 @@ song_table_create = ("""
 """)
 
 # artist table creation (artists in music database)
+# artist table schema
 artist_table_create = ("""
     CREATE TABLE IF NOT EXISTS artists (
         artist_id    VARCHAR(50) SORTKEY,
@@ -116,6 +122,7 @@ artist_table_create = ("""
 """)
 
 # time table (timestamps of records in songplays broken down into specific units)
+# time table schema
 time_table_create = ("""
     CREATE TABLE IF NOT EXISTS time (
         start_time TIMESTAMP  NOT NULL SORTKEY,
@@ -127,8 +134,10 @@ time_table_create = ("""
         weekday     VARCHAR(30) NOT NULL
     ) diststyle all;
 """)
-#==========================================================================================================
-# STAGING TABLES
+
+#===============================================================================
+# STAGING TABLES SOURCING
+# staging_events_copy from Amazon S3
 staging_events_copy = ("""
 COPY staging_events FROM {}
     CREDENTIALS 'aws_iam_role={}'
@@ -139,7 +148,7 @@ COPY staging_events FROM {}
             config.get('IAM_ROLE', 'ARN'), 
              config.get('S3', 'LOG_JSONPATH')
 )
-
+# staging_songs_copy from Amazon S3
 staging_songs_copy = ("""
     COPY staging_songs FROM {}
     credentials 'aws_iam_role={}'
@@ -150,10 +159,8 @@ staging_songs_copy = ("""
 """).format(config.get('S3', 'SONG_DATA'),
             config.get('IAM_ROLE', 'ARN'))
 
-
-
-# ==============================================================================================
-# FINAL TABLES RESOURCING
+# ==============================================================================
+# FINAL TABLES INSERTION-RESOURCING
 # FACT TABLE >> SONGPLAY
 songplay_table_insert = ("""
     INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
@@ -171,6 +178,7 @@ songplay_table_insert = ("""
     WHERE se.page = 'NextSong');   
 """)
 # Inserting data into users table
+# USERS TABLE
 user_table_insert = ("""
     INSERT INTO users (user_id, first_name, last_name, gender, level)
     (SELECT DISTINCT(userId)  AS user_id, 
@@ -182,6 +190,7 @@ user_table_insert = ("""
     WHERE page = 'NextSong' AND userId IS NOT NULL);
 """)
 # song_table_insert
+# SONGS TABLE
 song_table_insert = ("""
     INSERT INTO songs (song_id, title, artist_id, year, duration)
     (SELECT DISTINCT(song_id) AS song_id, 
@@ -194,6 +203,7 @@ song_table_insert = ("""
 """)
 
 # artist_table_insert
+# ARTIST TABLE
 artist_table_insert = ("""
     INSERT INTO artists (artist_id, name, location, latitude, longitude)
     (SELECT DISTINCT(artist_id) AS artist_id,
@@ -206,6 +216,7 @@ artist_table_insert = ("""
 """)
 
 # time_table_insert
+# TIME TABLE
 time_table_insert = ("""
     INSERT INTO time (start_time, hour, day, week, month, year, weekday)
     (SELECT ts                              AS start_time, 
@@ -219,7 +230,9 @@ time_table_insert = ("""
         FROM staging_events se     
     ));
 """)
-#==========Adding analytical queries======
+
+#==========Adding analytical queries========================================================================================
+# ANALYTICAL QUERIES
 analytical_queries = [
     # How many songs each user listened with the App
     'SELECT DISTINCT songplays.user_id, users.first_name, users.last_name, COUNT(songplays.artist_id) AS num_songs_listened\
@@ -266,7 +279,7 @@ analytical_queries = [
      ORDER BY num_listened DESC\
      LIMIT 10;'
 ]
-
+# QUERY TITLE
 analytical_query_titles = [
     'Number of songs each user listens to', 
     'Total hour spent by each user'
@@ -277,7 +290,8 @@ analytical_query_titles = [
 ]
 
 # QUERY LISTS
-#========================================================================================================
+#==================================================================================================
+# DROP TABLE QUEIRES LIST
 drop_table_queries = [
                       staging_events_table_drop,
                       staging_songs_table_drop, 
@@ -286,7 +300,7 @@ drop_table_queries = [
                       song_table_drop, 
                       artist_table_drop, 
                       time_table_drop]
-
+# CREATE TABLE QUERIES LIST
 create_table_queries = [staging_events_table_create,
                         staging_songs_table_create,
                         songplay_table_create, 
@@ -294,14 +308,16 @@ create_table_queries = [staging_events_table_create,
                         song_table_create, 
                         artist_table_create, 
                         time_table_create]
-
+# STAGING TABLES
 copy_table_order = ['staging_events', 
                     'staging_songs']
-
+# STAGING COPIES
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 
+# INSERT TABLE IN ORDER
 insert_table_order = ['artists', 'songs', 'time', 'users', 'songplays']
 
+# INSERTION IN ORDER
 insert_table_queries = [artist_table_insert, 
                         song_table_insert, 
                         time_table_insert, 
