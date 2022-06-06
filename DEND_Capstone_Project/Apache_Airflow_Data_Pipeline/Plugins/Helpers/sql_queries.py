967bdfca0f2c837ab2d10826097673d7ab4d3436
Ahmed Md Farhad <af31k@outlook.com>
2022-06-06 14:28:40 -0700

    Update sql_queries.py

--- DEND_Capstone_Project/Apache_Airflow_Data_Pipeline/Plugins/Helpers/sql_queries.py
+++ DEND_Capstone_Project/Apache_Airflow_Data_Pipeline/Plugins/Helpers/sql_queries.py
@@ -1,4 +1,5 @@
 class SqlQueries:
+    
     dim_Country_Codes_insert = ("""
           SELECT
                 Country_Codes,
@@ -39,6 +40,7 @@ class SqlQueries:
                 Avg_Temp
           FROM  Staging_US_City_Temperature uc
           INNER JOIN Fact_Immigration_Table ft ON uc.US_Port = ft.Port_Code
+          INNER JOIN Fact_Immigration_Table ft ON uc.Port_State = ft.Current_State..??
           
 """)
 
@@ -89,16 +91,15 @@ class SqlQueries:
                             Means_of_Travel,
                             Visa_Purpose
                       FROM  Staging_Fact_Immigration_Table im
-                      INNER JOIN  dim_Country_Codes AS cc  ON  im.Citizenship_Country = cc.Country_Codes
-                      INNER JOIN  dim_Country_Codes AS cr  ON  im.Residency_Country = cr.Country_Codes
-                      INNER JOIN  dim_State_Codes AS st    ON  im.Current_State = st.State_Codes
-                      INNER JOIN  dim_Port_Locations AS pl ON  im.Current_State = pl.Port_States
-                      INNER JOIN  dim_Port_Locations_df AS pc ON  im.Port_Code = pc.Port_Codes
-                      INNER JOIN  dim_Individual_Immigrants_Records ir  ON  im.Admission_Num = ir.Entry_Num
-                      INNER JOIN  dim_Dated_Arrival_Departure dt  ON im.Arrival_Date = dt.Entry_Date
-                      INNER JOIN  dim_US_City_Temperature uc ON im.Port_Code = uc.US_Port
-                      INNER JOIN  dim_US_City_Demog_Race  ur ON im.Port_Code = ur.US_Port
-           ------?
+                      LEFT JOIN  dim_Country_Codes AS cc  ON  im.Citizenship_Country = cc.Country_Codes
+                      LEFT JOIN  dim_Country_Codes AS cr  ON  im.Residency_Country = cr.Country_Codes
+                      LEFT JOIN  dim_State_Codes AS st    ON  im.Current_State = st.State_Codes
+                      LEFT JOIN  dim_Port_Locations AS pl ON  im.Current_State = pl.Port_States
+                      LEFT JOIN  dim_Port_Locations AS pc ON  im.Port_Code = pc.Port_Codes
+                      LEFT JOIN  dim_Individual_Immigrants_Records ir  ON  im.Admission_Num = ir.Entry_Num
+                      LEFT JOIN  dim_Dated_Arrival_Departure dt  ON im.Arrival_Date = dt.Entry_Date
+                      LEFT JOIN  dim_US_City_Temperature uc ON im.Port_Code = uc.US_Port
+                      LEFT JOIN  dim_US_City_Demog_Race  ur ON im.Port_Code = ur.US_Port
 """)
     
 #----------------------Individual_Immigrants_Records---------------------------
@@ -137,49 +138,3 @@ class SqlQueries:
 """)
     
 
-    
-    
-    
-#==============================================================================================================
-class SqlQueries:
-    songplay_table_insert = ("""
-        SELECT
-                md5(events.sessionid || events.start_time) songplay_id,
-                events.start_time, 
-                events.userid, 
-                events.level, 
-                songs.song_id, 
-                songs.artist_id, 
-                events.sessionid, 
-                events.location, 
-                events.useragent
-                FROM (SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, *
-            FROM staging_events
-            WHERE page='NextSong') events
-            LEFT JOIN staging_songs songs
-            ON events.song = songs.title
-                AND events.artist = songs.artist_name
-                AND events.length = songs.duration
-    """)
-
-    user_table_insert = ("""
-        SELECT distinct userid, firstname, lastname, gender, level
-        FROM staging_events
-        WHERE page='NextSong'
-    """)
-
-    song_table_insert = ("""
-        SELECT distinct song_id, title, artist_id, year, duration
-        FROM staging_songs
-    """)
-
-    artist_table_insert = ("""
-        SELECT distinct artist_id, artist_name, artist_location, artist_latitude, artist_longitude
-        FROM staging_songs
-    """)
-
-    time_table_insert = ("""
-        SELECT start_time, extract(hour from start_time), extract(day from start_time), extract(week from start_time), 
-               extract(month from start_time), extract(year from start_time), extract(dayofweek from start_time)
-        FROM songplays
-    """)
