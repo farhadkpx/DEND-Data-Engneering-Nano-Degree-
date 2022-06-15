65aa09d8b5515b74fa08eef37e0d698c40337172
Ahmed Md Farhad <af31k@outlook.com>
2022-06-14 23:05:41 -0700

    Update Clearing_Staging_Tables.sql

--- DEND_Capstone_Project/Apache_Airflow_Data_Pipeline/DAGs/Clearing_Staging_Tables.sql
+++ DEND_Capstone_Project/Apache_Airflow_Data_Pipeline/DAGs/Clearing_Staging_Tables.sql
@@ -1,1 +1,9 @@
+TRUNCATE TABLE public.Staging_Immigration_Fact_Table;
+TRUNCATE TABLE public.Staging_Individual_Immigrants_Records;
+TRUNCATE TABLE public.Staging_Dated_Arrival_Departure;
+TRUNCATE TABLE public.Staging_US_City_Temperature;
+TRUNCATE TABLE public.Staging_US_City_Demog_Race;
+TRUNCATE TABLE public.Staging_Port_Locations;
+TRUNCATE TABLE public.Staging_Country_Codes;
+TRUNCATE TABLE public.Staging_State_Codes;
 
